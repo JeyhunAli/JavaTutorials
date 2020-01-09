d5f2f4d3a851737331f6eb117060b49e45d353b2
Jey <jey@Jeys-MBP.home>
2020-01-09 16:01:53 -0500

    two new classess added

---
+++ src/java_tutorials/StringCongatinations_2.java
@@ -0,0 +1,38 @@
+package java_tutorials;
+
+public class StringCongatinations_2 {
+
+	public static void main(String[] args) {
+
+		
+		char h = 'a';
+		char h1 = 'b';
+	
+		System.out.println(h+h1);
+		
+		/**
+		 * the answer here will be 195 why because 
+		 * 
+		 * in char these are the ascii table values in every char has own number here a--> 97,
+		 * b --> 98 
+		 * 
+		 * ASCII ------ stands for American Standard Code for Information Interchange.
+		 * 
+		 * the range of the ascii values :
+		 * 
+		 * 0-9 --->> 48-57
+		 * a-z ---->> 97-122
+		 * A-Z ---->> 65-90
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		
+		
+		
+	}
+
+}
